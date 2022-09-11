304cb6d8b10658f92cc41ebc290c9482f92de960
Vladislav Shub <1196390+vladshub@users.noreply.github.com>
2022-09-11 15:31:38 +0300

    better shiffting

--- start.sh
+++ start.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-orig_args=$@
-for i in "$@"; do
-  echo $i
-  case $i in
-    -b=*|--build=*)
-      build_number="${i#*=}"
-      echo "Build Number $build_number" 
-      ;;
-  esac
-  shift
-done
+# better way to use shift, since shift applys only to arguments ($@) and not arbitrarry arrays https://tldp.org/LDP/Bash-Beginners-Guide/html/sect_09_07.html
+function processArgs {
+    while (( "$#" )); do
+        case $1 in
+            -b=*|--build=*)
+                build_number="${1#*=}"
+                echo "Build Number $build_number"
+                ;;
+        esac
+        shift
+    done
+}
 
-./next.sh $orig_args
+processArgs $@
+
+./next.sh $@
